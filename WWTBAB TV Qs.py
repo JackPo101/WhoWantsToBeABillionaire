0aa581ffdef2413619ed08b9e9200a4ceb48d9f8
GhostChar <charman.c@etoncollege.org.uk>
2016-05-16 10:59:51 +0100

    Update WWTBAB TV Qs.py
    Version 4.0

--- WWTBAB TV Qs.py
+++ WWTBAB TV Qs.py
@@ -1,72 +1,84 @@
 TVq1 = input('How many Dragons are there in "Dragons Den"?\na. 4\nb. 5\nc. 6\nd. 7\n')
-if TVq1 == 'B':
+upper = TVq1.upper()
+if upper == 'B':
     print('Correct! Next question...')
 else:
     print('Wrong answer! You lose!')
     exit()
 
 TVq2 = input('Jon Snow is a fictional character in what series?\na. Grimm\nb. The Tomorrow People\nc. Game of Thrones\nd. Falling Skies\n')
-if TVq2 == 'c':
+upper = TVq2.upper()
+if upper == 'C':
     print('Correct! Next question...')
 else:
     print('Wrong answer! You lose!')
     exit()
     
 TVq3 = input('In "Sherlock", Bendict Cumberbatch played Sherlock Holmes, but who played Doctor Watson?\na. Martin Freeman\nb. Brad Pitt\nc. Kevin Costner\nd. Ben Kingsley\n')
-if TVq3 == 'a':
+upper = TVq3.upper()
+if upper == 'A':
     print('Correct! Next question...')
 else:
     print('Wrong answer! You lose!')
     exit()
 
 TVq4 = input('Who hosts the topical celebrity panel show, "Mock the Week"?\na. Jimmy Carr\nb. Michael McIntyre\nc. Hugh Dennis\nd. Dara O Brien\n')
-if TVq4 == 'd':
+upper = TVq4.upper()
+if upper == 'D':
     print('Correct! Next question...')
 else:
     print('Wrong answer! You lose!')
     exit()
     
 TVq5 = input("What is the name of Simon Cowell's baby?\na. Hal\nb. Landon\nc. Eric\nd. Jett\n")
-if TVq5 == 'c':
+upper = TVq5.upper()
+if upper == 'C':
     print('Correct! Next question...')
 else:
     print('Wrong answer! You lose!')
     exit()
     
 TVq6 = input('How many wars were there before the popular British TV show, "Robot Wars", was axed?\na. 5\nb. 6\nc. 7\nd. 8\n')
-if TVq6 == 'c':
+upper = TVq6.upper()
+if upper == 'C':
     print('Correct! Next question...')
 else:
     print('Wrong answer! You lose!')
     exit()
 
 TVq7 = input('In "The Big Bang Theory", what is the middle name of Sheldon Cooper?\na. Harry\nb. Lee\nc. Trevor\nd. Austin\n')
-if TVq7 == 'b':
+upper = TVq7.upper()
+if upper == 'B':
     print('Correct! Next question...')
 else:
     print('Wrong answer! You lose!')
     exit()
 
 TVq8 = input('What is the middle name of Homer Simpson?\na. Jay\nb. John\nc. Jerry\nd. James\n')
-if TVq8 == 'a':
+upper = TVq8.upper()
+if upper == 'A':
     print('Correct! Next question...')
 else:
     print('Wrong answer! You lose!')
     exit()
 
 TVq9 = input('What is the real name of Keith Lemon?\na. Andy Brown\nb. Leigh Francis\nc. Bud Summerfield\nd. Zac Hart\n')
-if TVq9 == 'b':
+upper = TVq9.upper()
+if upper == 'b':
     print('Correct! Next question...')
 else:
     print('Wrong answer! You lose!')
     exit()
 
 TVq10 = input('In what year did the comedy film "Carry on Sergeant" release?\na. 1961\nb. 1953\nc. 1957\nd. 1958\n')
-if TVq10 == 'd':
+upper = TVq10.upper()
+if upper == 'd':
     print('Correct! You have won £1,000,000,000!!')
 else:
     print('Wrong answer! You lose!')
-    exit()
+
+
+
     
     
 
