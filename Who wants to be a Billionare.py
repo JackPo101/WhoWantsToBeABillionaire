13a211929f3e76ee539853f071c5d395747e0fea
gingerninja1401 <stephenson_garcia.a@etoncollege.org.uk>
2016-05-16 11:14:55 +0100

    Add files via upload

---
+++ Who wants to be a Billionare.py
@@ -0,0 +1,44 @@
+def q1():
+    q1 = input('What is the capital of Thailand: is it A-Beijing, B-Saigon, C-Bangkok, D-Phnom Penh:')
+    if q1 == 'C':
+        print('Well done you may proceed.')
+    else:
+        print('Bad Luck!')
+
+def q2():
+    q2 = input('Where did Moses get the 10 Commandments: is it A-Mount Sinai, B-Mount Cyanide, C-Egypt, D-Jerusalem: ')
+    if q2 == 'A':
+        print('Well done')
+    else:
+        print('You Suck')
+
+def q3():
+    q3 = input('What is Austria\'s national flower: is it A- Rose, B-Daffodil, C-Edelweiss, D-Lilac: ')
+    if q3 == 'C':
+        print('Well done')
+    else:
+        print('You Suck')
+
+def q4():
+    q4 = input('Who gave the Statue of Liberty to America: A-America, B-France, C-Britain, D-Spain:')
+    if q4 == 'B':
+        print('Well Done')
+    else:
+        print('You Suck')
+
+def q7():
+    q7 = input('Name two double landlocked countries: is it A-Kazahkstan and Switzerland, B-Lichstenstein and Uzbekistan, C-Uzbekistan and Tajikistan, D-Switzerland and Pakistan:')
+    if q7 == 'B':
+        print('Well Done')
+    else:
+        print('Unlucky')
+
+def millionDollarQuestion():
+    q10 = input('What game uses ELO to rank its players: A-Halo, B-CS:GO, C-League of Legends, D-DOTA: ')
+    if q10 == 'C':
+        print('Well done')
+    else:
+        print('Unlucky')
+    
+    
+
