dd8a3023a8a9d5ce2bed33532eeda0e6266e3dd0
GhostChar <charman.c@etoncollege.org.uk>
2016-05-16 07:23:45 +0100

    Add files via upload
    TV Questions done

---
+++ WWTBAB TV Qs.py
@@ -0,0 +1,73 @@
+TVq1 = input('How many wars were there before the popular British TV show, "Robot Wars", was axed?\na. 5\nb. 6\nc. 7\nd. 8\n')
+if TVq1 == 'c':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+TVq2 = input('In "Sherlock", Bendict Cumberbatch played Sherlock Holmes, but who played Doctor Watson?\na. Martin Freeman\nb. Brad Pitt\nc. Kevin Costner\nd. Ben Kingsley\n')
+if TVq2 == 'a':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+TVq3 = input('How many Dragons are there in "Dragons Den"?\na. 4\nb. 5\nc. 6\nd. 7\n')
+if TVq3 == 'b':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+TVq4 = input('In "The Big Bang Theory", what is the middle name of Sheldon Cooper?\na. Harry\nb. Lee\nc. Trevor\nd. Austin\n')
+if TVq4 == 'b':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+TVq5 = input('Jon Snow is a fictional character in what series?\na. Grimm\nb. The Tomorrow People\nc. Game of Thrones\nd. Falling Skies\n')
+if TVq5 == 'c':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+TVq6 = input('Who hosts the topical celebrity panel show, "Mock the Week"?\na. Jimmy Carr\nb. Michael McIntyre\nc. Hugh Dennis\nd. Dara O Brien\n')
+if TVq6 == 'd':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+TVq7 = input('What is the middle name of Homer Simpson?\na. Jay\nb. John\nc. Jerry\nd. James\n')
+if TVq7 == 'a':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+TVq8 = input('What is the real name of Keith Lemon?\na. Andy Brown\nb. Leigh Francis\nc. Bud Summerfield\nd. Zac Hart\n')
+if TVq8 == 'b':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+TVq9 = input("What is the name of Simon Cowell's baby?\na. Hal\nb. Landon\nc. Eric\nd. Jett\n")
+if TVq9 == 'c':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+TVq10 = input('In what year did the comedy film "Carry on Sergeant" release?\na. 1961\nb. 1953\nc. 1957\nd. 1958\n')
+if TVq10 == 'd':
+    print('Correct! You have won £1,000,000,000!!')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+    
+    
+
+
