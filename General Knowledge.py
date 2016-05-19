39607c093d2b1326ff00a2a2210b4d45f1ef98f3
gingerninja1401 <stephenson_garcia.a@etoncollege.org.uk>
2016-05-19 09:39:02 +0100

    Add files via upload
    Jack review questions and tell me what to change

---
+++ General Knowledge.py
@@ -0,0 +1,85 @@
+GKq1 = input('What is the capital of Thailand: is it A-Beijing, B-Saigon, C-Bangkok, D-Phnom Penh:')
+upper = GKq1.upper()
+if upper == 'C':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+GKq2 = input('Where did Moses get the 10 Commandments: is it A-Mount Sinai, B-Mount Cyanide, C-Egypt, D-Jerusalem: ')
+upper = GKq2.upper()
+if upper == 'A':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+    
+GKq3 = input('What is Austria\'s national flower: is it A- Rose, B-Daffodil, C-Edelweiss, D-Lilac: ')
+upper = GKq3.upper()
+if upper == 'C':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+GKq4 = input('Who gave the Statue of Liberty to America: A-America, B-France, C-Britain, D-Spain:')
+upper = GKq4.upper()
+if upper == 'B':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+    
+GKq5 = input("What are there allegedly none of in Ireland: A-Dogs, B-Beavers, C-Snakes, D-Potatoes")
+upper = GKq5.upper()
+if upper == 'C':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+    
+GKq6 = input('What percentage of a League of Legends player is salt: A-23% , B-99%, C-50%, D-0%')
+upper = GKq6.upper()
+if upper == 'B':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+GKq7 = input('Name two double landlocked countries: is it A-Kazahkstan and Switzerland, B-Lichstenstein and Uzbekistan, C-Uzbekistan and Tajikistan, D-Switzerland and Pakistan:')
+upper = GKq7.upper()
+if upper == 'B':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+GKq8 = input('What was The Shield: A-A WWE team, B-Excalibur\'s counterpart, C-A Marvel Team, D-Leonidas\' shield')
+upper = GKq8.upper()
+if upper == 'A':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+GKq9 = input('What was is a pegasus in Greek mythology: A-A flying horse, B-A three headed dog, C-A flying Lizard, D-A famous warrior who defeated a minotaur ')
+upper = GKq9.upper()
+if upper == 'A':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+GKq10 = input('What game uses ELO to rate it\'s players: A-Dota, B-League of Legends, C-CS:GO, D-Smite')
+upper = GKq10.upper()
+if upper == 'B':
+    print('Correct! You have won £1,000,000,000!!')
+else:
+    print('Wrong answer! You lose!')
+
+
+
+    
+    
+
+
