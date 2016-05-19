cec9eb0d04496bdabeb569758dd766228df79e57
GhostChar <charman.c@etoncollege.org.uk>
2016-05-19 09:33:07 +0100

    Update INT Qs WWTBAB.py
    Version 2.0 with choice ifs

--- INT Qs WWTBAB.py
+++ INT Qs WWTBAB.py
@@ -1,70 +1,80 @@
-INTq1 = input('Complete the popular vine title: "Damn, ______":\na. Cameron\nb. Daniel\nc. Derek\nd. Drake\n')
-upper = INTq1.upper()
-if upper == 'B':
-    print('Correct! Next question...')
-else:
-    print('Wrong answer! You lose!')
-    exit()
-INTq2 = input('Who is the most subscribed YouTuber on the website (excluding music channels)?\na. jacksepticeye\nb. Markiplier\nc. PewDiePie\nd. Smosh\n')
-upper = INTq2.upper()
-if upper == 'C':
-    print('Correct! Next question...')
-else:
-    print('Wrong answer! You lose!')
-    exit()
-INTq3 = input('Which of these is the title of a series of YouTube videos featuring animated stickman characters in many random clips?\na. qwer movies\nb. asdf movies\nc. zxcv movies\nd. 1qaz movies\n')
-upper = INTq3.upper()
-if upper == 'B':
-    print('Correct! Next question...')
-else:
-    print('Wrong answer! You lose!')
-    exit()
-INTq4 = input('"Bonnie", "Chica", and "Foxy" come from which extremely popular 2014 video game?\na. Undertale\nb. Skyrim\nc. Destiny\nd. Five Nights at Freddys\n')
-upper = INTq4.upper()
-if upper == 'D':
-    print('Correct! Next question...')
-else:
-    print('Wrong answer! You lose!')
-    exit()
-INTq5 = input('What does the internet meme "MLG" stand for?\na. Major League Gaming\nb. Major League Grandfinals\nc. Major League Green\nd. Meme Lord Gandalf\n')
-upper = INTq5.upper()
-if upper == 'A':
-    print('Correct! Next question...')
-else:
-    print('Wrong answer! You lose!')
-    exit()
-INTq6 = input('Who recieved "a small loan of a million dollars" from their father, thus creating a worldwide internet meme?\na. David Cameron\nb. Donald Trump\nc. Hillary Clinton\nd. Boris Johnson\n')
-upper = INTq6.uppper()
-if upper == 'C':
-    print('Correct! Next question...')
-else:
-    print('Wrong answer! You lose!')
-    exit()
-INTq7 = input('Complete the name of the popular meme: Bad Luck, ____\na. Brian\nb. Barry\nc. Bob\nd. Banter\n')
-upper = INTq7.uppper()
-if upper == 'A':
-    print('Correct! Next question...')
-else:
-    print('Wrong answer! You lose!')
-    exit()
-INTq8 = input('What is the name of the YouTube channel hosted by "Keemstar" that deals with "Drama"?\na. YouTubeDrama\nb. DramaAlert\nc. DramaKeem\nd. KeemstarDaily\n')
-upper = INTq8.uppper()
-if upper == 'B':
-    print('Correct! Next question...')
-else:
-    print('Wrong answer! You lose!')
-    exit()
-INTq9 = input('"Overly Attached Girlfriend" has her own YouTube channel. What is it called?\na. Leia\nb. OverlyAttachedGirlfriend\nc. OAGYouTube\nd. Laina\n')
-upper = INTq9.uppper()
-if upper == 'D':
-    print('Correct! Next question...')
-else:
-    print('Wrong answer! You lose!')
-    exit()
-INTq10 = input('scottgames.com, the website of the creator of the viral game, "Five Nights at Freddys", updated not long ago with a teaser to a new spin-off game called "Sister Location".\nWhat is the name of the animatronic featured in the teaser?\na. Baby\nb. Bidybab\nc. Ballora\nd. Ennard\n')
-upper = INTq10.uppper()
-if upper == 'A':
-    print('Correct! You have won £1,000,000,000!!')
-else:
-    print('Wrong answer! You lose!')
-    exit()
+if choice == 71:
+    INTq1 = input('Complete the popular vine title: "Damn, ______":\na. Cameron\nb. Daniel\nc. Derek\nd. Drake\n')
+    upper = INTq1.upper()
+    if upper == 'B':
+        print('Correct! Next question...')
+    else:
+        print('Wrong answer! You lose!')
+        exit()
+if choice == 72:
+    INTq2 = input('Who is the most subscribed YouTuber on the website (excluding music channels)?\na. jacksepticeye\nb. Markiplier\nc. PewDiePie\nd. Smosh\n')
+    upper = INTq2.upper()
+    if upper == 'C':
+        print('Correct! Next question...')
+    else:
+        print('Wrong answer! You lose!')
+        exit()
+if choice == 73:
+    INTq3 = input('Which of these is the title of a series of YouTube videos featuring animated stickman characters in many random clips?\na. qwer movies\nb. asdf movies\nc. zxcv movies\nd. 1qaz movies\n')
+    upper = INTq3.upper()
+    if upper == 'B':
+        print('Correct! Next question...')
+    else:
+        print('Wrong answer! You lose!')
+        exit()
+if choice == 74:
+    INTq4 = input('"Bonnie", "Chica", and "Foxy" come from which extremely popular 2014 video game?\na. Undertale\nb. Skyrim\nc. Destiny\nd. Five Nights at Freddys\n')
+    upper = INTq4.upper()
+    if upper == 'D':
+        print('Correct! Next question...')
+    else:
+        print('Wrong answer! You lose!')
+        exit()
+if choice == 75:
+    INTq5 = input('What does the internet meme "MLG" stand for?\na. Major League Gaming\nb. Major League Grandfinals\nc. Major League Green\nd. Meme Lord Gandalf\n')
+    upper = INTq5.upper()
+    if upper == 'A':
+        print('Correct! Next question...')
+    else:
+        print('Wrong answer! You lose!')
+        exit()
+if choice == 76:
+    INTq6 = input('Who recieved "a small loan of a million dollars" from their father, thus creating a worldwide internet meme?\na. David Cameron\nb. Donald Trump\nc. Hillary Clinton\nd. Boris Johnson\n')
+    upper = INTq6.uppper()
+    if upper == 'C':
+        print('Correct! Next question...')
+    else:
+        print('Wrong answer! You lose!')
+        exit()
+if choice == 77:
+    INTq7 = input('Complete the name of the popular meme: Bad Luck, ____\na. Brian\nb. Barry\nc. Bob\nd. Banter\n')
+    upper = INTq7.uppper()
+    if upper == 'A':
+        print('Correct! Next question...')
+    else:
+        print('Wrong answer! You lose!')
+        exit()
+if choice == 78:
+    INTq8 = input('What is the name of the YouTube channel hosted by "Keemstar" that deals with "Drama"?\na. YouTubeDrama\nb. DramaAlert\nc. DramaKeem\nd. KeemstarDaily\n')
+    upper = INTq8.uppper()
+    if upper == 'B':
+        print('Correct! Next question...')
+    else:
+        print('Wrong answer! You lose!')
+        exit()
+if choice == 79:
+    INTq9 = input('"Overly Attached Girlfriend" has her own YouTube channel. What is it called?\na. Leia\nb. OverlyAttachedGirlfriend\nc. OAGYouTube\nd. Laina\n')
+    upper = INTq9.uppper()
+    if upper == 'D':
+        print('Correct! Next question...')
+    else:
+        print('Wrong answer! You lose!')
+        exit()
+if choice == 80:
+    INTq10 = input('scottgames.com, the website of the creator of the viral game, "Five Nights at Freddys", updated not long ago with a teaser to a new spin-off game called "Sister Location".\nWhat is the name of the animatronic featured in the teaser?\na. Baby\nb. Bidybab\nc. Ballora\nd. Ennard\n')
+    upper = INTq10.uppper()
+    if upper == 'A':
+        print('Correct! You have won £1,000,000,000!!')
+    else:
+        print('Wrong answer! You lose!')
+        exit()
