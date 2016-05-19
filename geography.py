6007693998f9da0b49582267e65d8b745a521ab6
jWoNgz57 <wong217217217@gmail.com>
2016-05-19 09:16:34 +0100

    Add files via upload

---
+++ geography.py
@@ -0,0 +1,73 @@
+SCq1 = input('How many countries in Africa? \na. 0\nb. 34\nc. 51\nd. 54\n')
+if SCq1 == 'd':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+SCq2 = input('How many tectonic plates are there?\na. 1\nb. 4\nc. 7\nd. 9\n')
+if SCq2 == 'c':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+SCq3 = input('What is the capital of Azerbijan?\na. Afghanistan\nb. Swaziland\nc. Astana\nd. Baku\n')
+if SCq3 == 'd':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()a
+
+SCq4 = input('Which is the longest river in the world?\na. Amazon\nb. Nile\nc. Yellow River\nd. Thames\n')
+if SCq4 == 'b':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+SCq5 = input('What magnitude was the strongest earthquake ever recorded? (on the Richter scale) \na. 5.4\nb. 7\nc. 9.5\nd. 11\n')
+if SCq5 == 'a':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+SCq6 = input('Which African mainland country lies to the west of Madagascar: \na. Mozambique\nb. France\nc. India\nd. Maritius\n')
+if SCq6 == 'a':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+SCq7 = input('Ceuta is a Spanish enclaving a border with which country \na. Morocco\nb. Algeria\nc. Andorra\nd. France\n')
+if SCq7 == 'a':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+SCq8 = input('Angkor Wat, the largest religious monument in the world, is in which country? \na. Cambodia\nb. Russia\nc. Africa\nd. Damascus\n')
+if SCq8 == 'a':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+SCq9 = input("What is the capital of the Falkland Islands? \na. Jamestown\nb. London\nc. Beunos Aires\nd. Stanley\n")
+if SCq9 == 'd':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+SCq10 = input(' What is the capital of the Kiribati? \na. Apoiqwermcev \nb. South Tarawa\nc. North Tarawa\nd. Uban Bator\n')
+if SCq10 == 'd':
+    print('Correct! You have won £1,000,000,000!!')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+    
+    
+
+
