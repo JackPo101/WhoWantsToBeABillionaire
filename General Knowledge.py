4ac68e4052626865c1b43967906329c028f55b01
gingerninja1401 <stephenson_garcia.a@etoncollege.org.uk>
2016-05-23 10:45:41 +0100

    Update General Knowledge.py

--- General Knowledge.py
+++ General Knowledge.py
@@ -1,4 +1,4 @@
-GKq1 = input('What is the capital of Thailand: is it A-Beijing, B-Saigon, C-Bangkok, D-Phnom Penh:')
+GKq1 = input('What is the capital of Thailand: is it \nA-Beijing, \nB-Saigon, \nC-Bangkok, \nD-Phnom Penh:')
 upper = GKq1.upper()
 if upper == 'C':
     print('Correct! Next question...')
@@ -6,7 +6,7 @@ else:
     print('Wrong answer! You lose!')
     exit()
 
-GKq2 = input('Where did Moses get the 10 Commandments: is it A-Mount Sinai, B-Mount Cyanide, C-Egypt, D-Jerusalem: ')
+GKq2 = input('Where did Moses get the 10 Commandments: is it \nA-Mount Sinai, \nB-Mount Cyanide, \nC-Egypt, \nD-Jerusalem: ')
 upper = GKq2.upper()
 if upper == 'A':
     print('Correct! Next question...')
@@ -14,7 +14,7 @@ else:
     print('Wrong answer! You lose!')
     exit()
     
-GKq3 = input('What is Austria\'s national flower: is it A- Rose, B-Daffodil, C-Edelweiss, D-Lilac: ')
+GKq3 = input('What is Austria\'s national flower: is it \nA- Rose, \nB-Daffodil, \nC-Edelweiss, \nD-Lilac: ')
 upper = GKq3.upper()
 if upper == 'C':
     print('Correct! Next question...')
@@ -22,7 +22,7 @@ else:
     print('Wrong answer! You lose!')
     exit()
 
-GKq4 = input('Who gave the Statue of Liberty to America: A-America, B-France, C-Britain, D-Spain:')
+GKq4 = input('Who gave the Statue of Liberty to America: \nA-America, \nB-France, \nC-Britain, \nD-Spain:')
 upper = GKq4.upper()
 if upper == 'B':
     print('Correct! Next question...')
@@ -30,7 +30,7 @@ else:
     print('Wrong answer! You lose!')
     exit()
     
-GKq5 = input("What are there allegedly none of in Ireland: A-Dogs, B-Beavers, C-Snakes, D-Potatoes")
+GKq5 = input("What are there allegedly none of in Ireland: \nA-Dogs, \nB-Beavers, \nC-Snakes, \nD-Potatoes")
 upper = GKq5.upper()
 if upper == 'C':
     print('Correct! Next question...')
@@ -38,7 +38,7 @@ else:
     print('Wrong answer! You lose!')
     exit()
     
-GKq6 = input('What is Sasha Grey famous from: A-Porn, B-Television, C-Film industry, D-Singing')
+GKq6 = input('What is Sasha Grey famous from: \nA-Porn, \nB-Television, \nC-Film industry, \nD-Singing')
 upper = GKq6.upper()
 if upper == 'a':
     print('Correct! I bet you regret getting that right anyway next question...')
@@ -46,7 +46,7 @@ else:
     print('Wrong answer! Was it worth you reputation for the loss?')
     exit()
 
-GKq7 = input('Name two double landlocked countries: is it A-Kazahkstan and Switzerland, B-Lichstenstein and Uzbekistan, C-Uzbekistan and Tajikistan, D-Switzerland and Pakistan:')
+GKq7 = input('Name two double landlocked countries: is it \nA-Kazahkstan and Switzerland, \nB-Lichstenstein and Uzbekistan, \nC-Uzbekistan and Tajikistan, \nD-Switzerland and Pakistan:')
 upper = GKq7.upper()
 if upper == 'B':
     print('Correct! Next question...')
@@ -54,7 +54,7 @@ else:
     print('Wrong answer! You lose!')
     exit()
 
-GKq8 = input('What was The Shield: A-A WWE team, B-Excalibur\'s counterpart, C-A Marvel Team, D-Leonidas\' shield')
+GKq8 = input('What was The Shield: \nA-A WWE team, \nB-Excalibur\'s counterpart, \nC-A Marvel Team, \nD-Leonidas\' shield')
 upper = GKq8.upper()
 if upper == 'A':
     print('Correct! Next question...')
@@ -62,7 +62,7 @@ else:
     print('Wrong answer! You lose!')
     exit()
 
-GKq9 = input('What was is a pegasus in Greek mythology: A-A flying horse, B-A three headed dog, C-A flying Lizard, D-A famous warrior who defeated a minotaur ')
+GKq9 = input('What was is a pegasus in Greek mythology: \nA-A flying horse, \nB-A three headed dog, \nC-A flying Lizard, \nD-A famous warrior who defeated a minotaur ')
 upper = GKq9.upper()
 if upper == 'A':
     print('Correct! Next question...')
@@ -70,7 +70,7 @@ else:
     print('Wrong answer! You lose!')
     exit()
 
-GKq10 = input('What game uses ELO to rate it\'s players: A-Dota, B-League of Legends, C-CS:GO, D-Smite')
+GKq10 = input('What game uses ELO to rate it\'s players: \nA-Dota, \nB-League of Legends, \nC-CS:GO, \nD-Smite')
 upper = GKq10.upper()
 if upper == 'B':
     print('Correct! You have won £1,000,000,000!!')
