bca5e7640b2f80e7ddfe6ceb5e824746c84fae92
napiersmiths <napier-smith.s@etoncollege.org.uk>
2016-05-23 10:45:27 +0100

    Add files via upload

---
+++ ARTS AND LITERATURE NO DEF.py
@@ -0,0 +1,88 @@
+if choice == 91:
+    ALq1 = input('What is the second book of the Old Testament?\na. James\nb. Exodus\nc. Acts\nd. Revelation\n')
+    upper = ALq1.upper()
+if upper == 'B':
+     print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+if choice == 92:
+    ALq2 = input('Complete the title of this book. "Down and out in..?\na. Detroit and New York\nb. London and Manchester\nc. Delhi and Paris\nd. Paris and London\n')
+    upper = ALq2.upper()
+if upper == 'D':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+if choice == 93:
+    ALq3 = input('In what wood does Winnie the Pooh live?\na. Hundred Acre Wood\nb. Endor\nc. Forbidden Forest\nd. The forest of Arden\n')
+    upper = ALq3.upper()
+if upper == 'A':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+if choice == 94:
+    ALq4 = input('How many years does the toad in "Wind in the Willows" receive in jail?\na. 10\nb. 50\nc. 15\nd. 20\n')
+    upper = ALq4.upper()
+if upper == 'D':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+if choice == 95:
+    ALq5 = input('Who is the god of wine and drink in greek mythology?\na. Zeus\nb. Echo\nc. Dionysus\nd. Nike\n')
+    upper = ALq5.upper()
+if upper == 'C':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+if choice == 96:
+    ALq6 = input('What was the street address of Sherlock Holmes?\na. 343B Baker Street\nb. 221B Baker Street\nc. 1A Baker Street\nd. 211B Baker Street\n')
+    upper = ALq6.upper()
+if upper == 'B':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+if choice == 97:
+    ALq7 = input('Which London road is associated with book sellers?\na. Edgeware Road\nb. Gresham Street\nc. Fenchurch Street\nd. Charing Cross Road\n')
+    upper = ALq7.upper()
+if upper == 'D':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+if choice == 98:
+    ALq8 = input(' "Call me Ishmael" is the opening line of what novel?\na. Moby Dick\nb. Persuasion\nc. The Scarlet Letter\nd. Oliver Twist\n')
+    upper = ALq8.upper()
+if upper == 'A':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+if choice == 99:
+    ALq9 = input('The uniforms of what Italian military force were originally designed by Michelangelo?\na. Condottieri\nb. Vatican Swiss Guards\nc. Swiss Mercenaries\nd. Italian National Guard\n')
+    upper = ALq9.upper()
+if upper == 'B':
+    print('Correct! Next question...')
+else:
+    print('Wrong answer! You lose!')
+    exit()
+
+if choice == 100:
+    ALq10 = input('What 19th century French impressionist painted The Boating Party?\na. Degas\nb. Manet\nc. Pissaro\nd. Renoir\n')
+    upper = ALq10.upper()
+if upper == 'D':
+    print('Correct! You have won £1,000,000,000!!')
+else:
+    print('Wrong answer! You lose!')
